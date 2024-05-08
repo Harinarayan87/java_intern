4753eae84837f134a054e580ec96c3aeb9ae3bcd
Harinarayan Chaudhary <harinarayanchaudhari87@gmail.com>
2024-05-08 12:32:36 +0545

    second tryafter adding volume method

--- src/intern_project/areavolume.java
+++ src/intern_project/areavolume.java
@@ -1,15 +1,29 @@
 package intern_project;
 
 public class areavolume {
-static int getArea(int length,int breadth) {
-int area= length*breadth;	
-return area;
-}
+    //create method getArea
+	static int getArea(int length,int breadth) {
+    int area= length*breadth;	
+    return area;
+    }
+    //create method volume
+	static void volume(int area,int height) {
+		int vol = area * height;
+		System.out.println("The value of area is:"+area);
+		System.out.println("The value of volume is:"+vol);
+	}
+
+
 public static void main(String[] args) {
+	//define length and breadth
 	int length= 5;
 	int breadth= 3;
+	int height = 7;
+	//call method getArea
 	int area = getArea(length, breadth);
-	System.out.println("Area of the given is: "+area);
+	//System.out.println("Area of the given is: "+area);
+	//calling method volume
+	volume(area, height);
 }
 
 
