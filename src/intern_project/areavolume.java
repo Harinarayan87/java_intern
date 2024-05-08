8de8d6faf05650bb74b45fce5e3a553422466b32
Harinarayan Chaudhary <harinarayanchaudhari87@gmail.com>
2024-05-08 12:16:06 +0545

    checking area output

--- src/intern_project/areavolume.java
+++ src/intern_project/areavolume.java
@@ -1,5 +1,16 @@
 package intern_project;
 
 public class areavolume {
+static int getArea(int length,int breadth) {
+int area= length*breadth;	
+return area;
+}
+public static void main(String[] args) {
+	int length= 5;
+	int breadth= 3;
+	int area = getArea(length, breadth);
+	System.out.println("Area of the given is: "+area);
+}
+
 
 }
