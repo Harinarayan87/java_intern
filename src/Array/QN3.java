a809f9e0d8c9f70eb820116392f4f99e806008fb
Harinarayan Chaudhary <harinarayanchaudhari87@gmail.com>
2024-05-09 18:12:41 +0545

    first try but its output like 1,0,3,0,5

--- src/Array/QN3.java
+++ src/Array/QN3.java
@@ -1,5 +1,20 @@
 package Array;
 
 public class QN3 {
+	static void oddnumbers() {
+		int odd[] = new int[100];
+		for(int i=0; i<odd.length; i++) {
+			if(i%2!=0) {
+				odd[i]=i;
+			}}
+		System.out.println("Odd Numbers from 0 to 100 are:");	
+		for(int i=0; i<odd.length; i++) {
+			System.out.println(odd[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		oddnumbers();
+	}
 
 }
