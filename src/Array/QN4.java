ca88687dda8dafc02155b85cecbbbe831356a834
Harinarayan Chaudhary <harinarayanchaudhari87@gmail.com>
2024-05-09 19:38:37 +0545

    QN4 completed

---
+++ src/Array/QN4.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class QN4 {
+	static void evennumbers() {
+		int even[] = new int[50];
+		int n=0;
+		for(int i=0; i<100; i++) {
+			if(i%2==0) {
+				even[n]=i;
+				++n;
+			}}
+		System.out.println("Odd Numbers from 0 to 100 are:");	
+		for(int i=0; i<even.length; i++) {
+			System.out.println(even[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		evennumbers();
+	}
+
+}
