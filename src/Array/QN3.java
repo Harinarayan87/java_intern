13c3604f1c2dc5d1b62ffba81868998a4edc64eb
Harinarayan Chaudhary <harinarayanchaudhari87@gmail.com>
2024-05-09 19:30:04 +0545

    now get the expected output by changing how the loop size is defining

--- src/Array/QN3.java
+++ src/Array/QN3.java
@@ -2,10 +2,12 @@ package Array;
 
 public class QN3 {
 	static void oddnumbers() {
-		int odd[] = new int[100];
-		for(int i=0; i<odd.length; i++) {
+		int odd[] = new int[50];
+		int n=0;
+		for(int i=0; i<100; i++) {
 			if(i%2!=0) {
-				odd[i]=i;
+				odd[n]=i;
+				++n;
 			}}
 		System.out.println("Odd Numbers from 0 to 100 are:");	
 		for(int i=0; i<odd.length; i++) {
@@ -18,3 +20,4 @@ public class QN3 {
 	}
 
 }
+
